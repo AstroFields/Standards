6457b1c48bb82b7cd6e44cb43e71e05983363713
Kaiser Franz Josef <wecodemore@gmail.com>
2014-07-12 17:28:26 +0200

    Initial commit to GitHub

---
+++ Bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace WCM;
+
+/**
+ * Plugin Name: (WCM) AstroFields Standards
+ * Description: Standards Templates Extension for the AstroFields framework
+ */
+
+// Drop in Composer autoloader
+require_once plugin_dir_path( __FILE__ )."vendor/autoload.php";
